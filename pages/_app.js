42efcd1313d162180c3c35ff386539850cf62492
danial.asaria <dasaria@usc.edu>
2022-12-20 13:01:33 -0800

    hopefully fizing adsense script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import "../styles/globals.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import Head from "next/head"
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   
   return (
     <>
-      <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4671714996037159"
-            crossorigin="anonymous">
-        </script>
-      </Head>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4671714996037159" />
       <GoogleAnalytics trackPageViews gaMeasurementId={"G-W621EQN0KS"} />
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -90,10 +90,6 @@ export default function Home() {
         crossorigin="anonymous"
       >
       </script>
-      <link rel="apple-touch-icon" sizes="180x180" href=".././public/favicon_io/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href=".././public/favicon_io/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href=".././public/favicon_io/favicon-16x16.png" />
-      <link rel="manifest" href=".././public/favicon_io/site.webmanifest" />
     </Head>
       <Header />
       <div className={styles.largerContainer}>
