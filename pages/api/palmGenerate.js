cd477f11c036e5909c99edd294e5d7f883dfc0ab
danial <danialasaria@gmail.com>
2024-01-05 14:40:52 -0800

    using gemini instead of palm now

--- pages/api/palmGenerate.js
+++ pages/api/palmGenerate.js
@@ -1,26 +1,49 @@
-const { TextServiceClient } = require("@google-ai/generativelanguage").v1beta2;
-const { GoogleAuth } = require("google-auth-library");
+// const { TextServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// const { GoogleAuth } = require("google-auth-library");
+// const { ModelServiceClient } = require("@google-ai/generativelanguage");
 
 // Set up the model and API key for PaLM
-const MODEL_NAME = "models/text-bison-001";
-const API_KEY = process.env.PALM_API_KEY; // Ensure your API key is set in the environment variables
+// const MODEL_NAME = "models/gemini-pro";
+// const API_KEY = process.env.PALM_API_KEY; // Ensure your API key is set in the environment variables
+const genAI = new GoogleGenerativeAI(process.env.PALM_API_KEY);
+const newGeminiModel = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-const client = new TextServiceClient({
-  authClient: new GoogleAuth().fromAPIKey(API_KEY),
-});
+// const client = new TextServiceClient({
+//   authClient: new GoogleAuth().fromAPIKey(API_KEY),
+// });
+
+// const modelListClient = new ModelServiceClient({
+//   authClient: new GoogleAuth().fromAPIKey(API_KEY),
+// });
+
+// async function listAvailableModels() {
+//   try {
+//     const models = await modelListClient.listModels();
+//     models.forEach((model) => console.log(model));
+//   } catch (error) {
+//     console.error("Error listing models:", error);
+//   }
+// }
 
 // Function to call the PaLM API for text generation
 async function generateTextWithPaLM(prompt) {
   try {
-    const response = await client.generateText({
-      model: MODEL_NAME,
-      prompt: {
-        text: prompt,
-      },
-    });
+    // listAvailableModels();
+    const response = await newGeminiModel.generateContent(
+      // {
+      prompt
+      // model: MODEL_NAME,
+      // prompt: {
+      //   text: prompt,
+      // },
+      // }
+    );
 
     // Assuming the first candidate's output is what we need
-    const generatedText = response[0].candidates[0].output;
+    // const generatedText = response[0].candidates[0].output;
+    const generatedText = response.response.text();
     return generatedText;
   } catch (error) {
     console.error("Error calling PaLM API:", error);
@@ -30,7 +53,7 @@ async function generateTextWithPaLM(prompt) {
 
 // Function to generate the AI prompt for a cover letter
 function generateAIPrompt(resume, jobTitle, jobDescription) {
-  return `Write a sophisticated cover letter that highlights the candidate's qualifications and experience from their resume for the indicated job title. In your letter, address how the candidate's skills align with the requirements of the job description, and provide specific examples of their experience. Additionally, discuss how the candidate's skills can contribute to the success of the company, and demonstrate their passion for their field. Base everything in the letter on the following Job Title, Job Description, and Resume: \n\n Job Title: ${jobTitle} \n\n Job Description: ${jobDescription}
+  return `Write a sophisticated cover letter as specific as possible to the companies mission, without fluff, that is 3-4 paragraphs that highlights the candidate's qualifications and experience from their resume for the indicated job title. In your letter, address how the candidate's skills align with the requirements of the job description, and provide specific examples of their experience. Additionally, discuss how the candidate's skills can contribute to the success of the company, why this job, and demonstrate their passion for their field. Base everything in the letter on the following Job Title, Job Description, and Resume: \n\n Job Title: ${jobTitle} \n\n Job Description: ${jobDescription}
   \n\n Resume: ${trimSpaces(
     resume
   )} \n\n Don't autocomplete the resume, focus on the cover letter.`;
