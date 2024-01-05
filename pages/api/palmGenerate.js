319ff6219de05df12a56cfbb8a7c4b65ee5788e4
danial <danialasaria@gmail.com>
2024-01-05 13:37:15 -0800

    only bison 1 works right now

--- pages/api/palmGenerate.js
+++ pages/api/palmGenerate.js
@@ -2,7 +2,7 @@ const { TextServiceClient } = require("@google-ai/generativelanguage").v1beta2;
 const { GoogleAuth } = require("google-auth-library");
 
 // Set up the model and API key for PaLM
-const MODEL_NAME = "models/text-bison";
+const MODEL_NAME = "models/text-bison-001";
 const API_KEY = process.env.PALM_API_KEY; // Ensure your API key is set in the environment variables
 
 const client = new TextServiceClient({
@@ -23,7 +23,7 @@ async function generateTextWithPaLM(prompt) {
     const generatedText = response[0].candidates[0].output;
     return generatedText;
   } catch (error) {
-    console.error('Error calling PaLM API:', error);
+    console.error("Error calling PaLM API:", error);
     throw error;
   }
 }
@@ -31,7 +31,9 @@ async function generateTextWithPaLM(prompt) {
 // Function to generate the AI prompt for a cover letter
 function generateAIPrompt(resume, jobTitle, jobDescription) {
   return `Write a sophisticated cover letter that highlights the candidate's qualifications and experience from their resume for the indicated job title. In your letter, address how the candidate's skills align with the requirements of the job description, and provide specific examples of their experience. Additionally, discuss how the candidate's skills can contribute to the success of the company, and demonstrate their passion for their field. Base everything in the letter on the following Job Title, Job Description, and Resume: \n\n Job Title: ${jobTitle} \n\n Job Description: ${jobDescription}
-  \n\n Resume: ${trimSpaces(resume)} \n\n Don't autocomplete the resume, focus on the cover letter.`;
+  \n\n Resume: ${trimSpaces(
+    resume
+  )} \n\n Don't autocomplete the resume, focus on the cover letter.`;
 }
 
 // Function to trim spaces from a string
@@ -52,6 +54,6 @@ export default async function handler(req, res) {
     const coverLetter = await generateTextWithPaLM(prompt);
     res.status(200).json({ result: coverLetter });
   } catch (error) {
-    res.status(500).send('Error generating text with PaLM');
+    res.status(500).send("Error generating text with PaLM");
   }
 }
